e52ec6781bda93879ae309c2f1398d0a0210ecc2
SaakarX <37829127+saakarx@users.noreply.github.com>
2024-09-19 14:07:28 +0530

    added icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+import DeleteIcon from './icon-delete';
+import EditIcon from './icon-edit';
+import LoaderIcon from './icon-loader';
+import MinusIcon from './icon-minus';
+import PlusIcon from './icon-plus';
+import ReplyIcon from './icon-reply';
+
+export { DeleteIcon, EditIcon, LoaderIcon, MinusIcon, PlusIcon, ReplyIcon };
--- src/types.type.ts
+++ src/types.type.ts
@@ -1,20 +1,27 @@
-export type UserType = {
-  image: { png: string; webp: string };
+export type User = {
+  id: string;
   username: string;
+  image: { png: string; webp: string };
 };
+
 export type ReplyType = {
   id: number;
   content: string;
   createdAt: string;
   score: number;
   replyingTo: string;
-  user: UserType;
+  user: User;
 };
+
 export type CommentType = {
   id: number;
   content: string;
   replies: ReplyType[];
   score: number;
   createdAt: string;
-  user: UserType;
+  user: User;
+};
+
+export type IconProps = {
+  className?: React.HTMLAttributes<SVGElement>['className'];
 };
