d43c911e9883631486bc93abbf6e6e481e7be00e
SaakarX <37829127+saakarx@users.noreply.github.com>
2024-09-19 13:53:49 +0530

    rename tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import { type Config } from 'tailwindcss';
 
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -9,41 +9,41 @@ module.exports = {
         white: 'hsl(0, 0%, 100%)',
         red: {
           soft: 'hsl(358, 79%, 66%)',
-          pale: 'hsl(357, 100%, 86%)'
+          pale: 'hsl(357, 100%, 86%)',
         },
         blue: {
           moderate: 'hsl(238, 40%, 52%)',
           lightGrayish: 'hsl(239, 57%, 85%)',
           dark: 'hsl(212, 24%, 26%)',
-          grayish: 'hsl(211, 10%, 45%)'
+          grayish: 'hsl(211, 10%, 45%)',
         },
         gray: {
           light: 'hsl(223, 19%, 93%)',
-          veryLight: 'hsl(228, 33%, 97%)'
-        }
+          veryLight: 'hsl(228, 33%, 97%)',
+        },
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
       },
       gridTemplateColumns: {
         'comment-desktop': 'auto 1fr auto',
         'comment-mobile': 'auto 1fr',
         'comment-create-mobile': '1fr auto',
-        'comment-create-desktop': '40px 1fr auto'
+        'comment-create-desktop': '40px 1fr auto',
       },
       gridTemplateRows: {
         'comment-desktop': 'auto 1fr',
         'comment-mobile': 'auto 1fr auto',
         'comment-create-mobile': '1fr auto',
-        'comment-create-desktop': 'auto'
+        'comment-create-desktop': 'auto',
       },
       maxWidth: {
-        '45.75rem': '45.75rem'
+        container: '45.75rem',
       },
       screens: {
-        md: '700px'
-      }
-    }
+        md: '700px',
+      },
+    },
   },
-  plugins: []
-};
+  plugins: [],
+} satisfies Config;
